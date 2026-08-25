68496ee9c024e3ec2aeb2d91996ed9f5dc56ab18
meteladrian-ship-it <meteladrian@gmail.com>
2026-08-25 14:46:47 +0200

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -4,16 +4,22 @@ import pyautogui
 
 pyautogui.FAILSAFE = True
 
-# 1. Uruchomienie programu ITAC przy użyciu pełnej ścieżki
+# 1. Uruchomienie programu ITAC
 subprocess.Popen(
     r"C:\Users\ametel\Desktop\ITAC Rework Tools\ITAC Rework Tools.exe"
 )
 
-# 2. Odczekaj 4 sekundy, aż okno logowania pojawi się na ekranie
+# 2. Odczekaj 4 sekundy na załadowanie okna
 time.sleep(4)
 
-# 3. Wpisanie loginu i hasła
+# 3. Klikamy w pole użytkownika (zakładamy, że okno otwiera się na środku ekranu lub po prostu używamy bezpiecznych tabulatorów)
+# Wyczyśćmy pole i wpiszemy login
+pyautogui.press('tab') # upewnienie się co do fokusu
 pyautogui.write("cze2.lead")
-pyautogui.press("tab")  # Przejście do pola hasła
+
+# 4. Przejście do pola hasła
+pyautogui.press("tab")
+
+# 5. Wpisanie hasła i zatwierdzenie
 pyautogui.write("PASSWORD")
 pyautogui.press("enter")
